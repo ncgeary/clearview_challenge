d5feb69b6d5aef716c7ee8eb077b4b395ed9f132
Nicholas Geary <ncgeary@buffalo.edu>
2019-04-30 17:28:36 -0400

    Updates

---
+++ precache-manifest.123e65325df7857c8490270a8d56f82b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ed3ec152cd48b8e59a81b116a473c8",
+    "url": "/clearview_challenge/index.html"
+  },
+  {
+    "revision": "43cbd301adf71102cfa0",
+    "url": "/clearview_challenge/static/css/main.fbf3b601.chunk.css"
+  },
+  {
+    "revision": "200833a7cb1e5494b8f8",
+    "url": "/clearview_challenge/static/js/2.ef57340a.chunk.js"
+  },
+  {
+    "revision": "43cbd301adf71102cfa0",
+    "url": "/clearview_challenge/static/js/main.3f0f1778.chunk.js"
+  },
+  {
+    "revision": "1544ff56dd42291caa46",
+    "url": "/clearview_challenge/static/js/runtime~main.02133d42.js"
+  }
+]);
